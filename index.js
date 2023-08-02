1df112dd4e9e3d3d83fbbe3d268a8ed607225b5e
Arpita Roy <arpita.roy.workar@gmail.com>
2023-08-02 16:50:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://prodb-a82d1-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "posts")
